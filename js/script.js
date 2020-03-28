00bf65c3abab09fac1bf4b6c5a6abfcb38473d06
bhanu k <talk2kish@gmail.com>
2020-03-28 12:37:39 +0530

    ENH: sort by date

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var misc_categories_uniq
 var url = "https://docs.google.com/spreadsheets/d/1_JIaWdsm6fYPStN2fin0wt4ad2JSn8Sqt5bNeBZ4riM/edit?usp=sharing"
 var g1_url = g1.url.parse(location.href)
+var sheets_global
 
 /**
   * hide subset of state cards based on URL param or search param
@@ -21,44 +22,59 @@ function toggle_state_cards(val, param) {
 }
 
 /**
-  * fetch data from google spreadsheet, prepare for the page
+  * Prepare the page
+  * @param {string} arrange_by
 */
-function init() {
+function render_page(arrange_by) {
+  $('.events-counter').html(sheets_global.length + ' updates')
+  if(arrange_by === 'date') {
+    filtered_elements_dates = _.sortBy(
+      _.each(sheets_global, function(item) { return item['dt'] = new Date(Number(Date.parse(item.when).toString()))
+    }), 'dt').reverse()
+    by_date = _.groupBy(filtered_elements_dates, "when")
+    data = {data: by_date, _by: 'date'}
+  } else {  // state
+    by_location = _.groupBy(sheets_global, "location")
+    g_sorted = _(by_location).toPairs().sortBy(0).fromPairs().value()
+    data = {data: g_sorted, _by: 'state'}
+  }
+  var tmpl = _.template($("#item-template").html())
+  $('#updates').html(tmpl(data))
+  render_subset() // if applicable based on URL param
+
+  misc_categories = _.uniq(_.map(sheets_global, function(item) { return item.misc }))
+  m = _.map(misc_categories, function(item) { return item.split(',') })
+  misc_categories_uniq = _.uniq([].concat.apply([], m))
+  // trim " PDS" to "PDS" (and others), take a unique set of them
+  misc_categories_uniq = _.uniq(_.map(misc_categories_uniq, function(item) { return item.trim() }))
+  $('#autocomplete').autocomplete({
+    source: misc_categories_uniq,
+    minLength: 0,
+    select: function(evt, ui) {
+      // search by category
+      toggle_state_cards(ui.item.value, 'cat')
+      g1_url.update({cat: ui.item.value, view: null}, 'cat=toggle&view=del')
+      history.pushState({}, '', g1_url.toString())
+      $("[data-label]").removeClass('border-selected')
+    }
+  }).bind('focus', function () {
+    // binds focus with autocomplete
+    $(this).autocomplete("search")
+  })
+}
+
+/**
+  * fetch data from google spreadsheet
+*/
+function init(arrange_by) {
   Tabletop.init({
     key: url,
     callback: function(sheets_data, tabletop) {
       // data in events sheet
-      filtered_elements = _.filter(sheets_data.events.elements, function(each_row) {
+      sheets_global = _.filter(sheets_data.events.elements, function(each_row) {
         return each_row['status'] != "0";
       })
-      $('.events-counter').html(filtered_elements.length + ' updates')
-      g = _.groupBy(filtered_elements,
-          "location")
-      g_sorted = _(g).toPairs().sortBy(0).fromPairs().value()
-      data = {data: g_sorted}
-      var tmpl = _.template($("#item-template").html())
-      $('#updates').html(tmpl(data))
-      render_subset() // if applicable based on URL param
-
-      misc_categories = _.uniq(_.map(filtered_elements, function(item) { return item.misc }))
-      m = _.map(misc_categories, function(item) { return item.split(',') })
-      misc_categories_uniq = _.uniq([].concat.apply([], m))
-      // trim " PDS" to "PDS" (and others), take a unique set of them
-      misc_categories_uniq = _.uniq(_.map(misc_categories_uniq, function(item) { return item.trim() }))
-      $('#autocomplete').autocomplete({
-        source: misc_categories_uniq,
-        minLength: 0,
-        select: function(evt, ui) {
-          // search by category
-          toggle_state_cards(ui.item.value, 'cat')
-          g1_url.update({cat: ui.item.value, view: null}, 'cat=toggle&view=del')
-          history.pushState({}, '', g1_url.toString())
-          $("[data-label]").removeClass('border-selected')
-        }
-      }).bind('focus', function () {
-        // binds focus with autocomplete
-        $(this).autocomplete("search")
-      })
+      render_page()
     },
     simpleSheet: false
   })
@@ -66,7 +82,7 @@ function init() {
   $('body').urlfilter({target: 'pushState'})
 
   var elems = document.querySelectorAll('.sidenav')
-  var instances = M.Sidenav.init(elems)
+  M.Sidenav.init(elems)
 }
 window.addEventListener('DOMContentLoaded', init)
 
@@ -93,9 +109,11 @@ $('body').on('click', 'a.btn', function() {
   $("#autocomplete").val('')
   $(this).addClass('border-selected')
   toggle_state_cards(label, 'view')
-
   g1_url.update({view: label, cat: null}, 'view=toggle&cat=del')
   history.pushState({}, '', g1_url.toString())
+}).on('click', '[data-arrange]', function() {
+  _by = $(this).data('arrange')
+  render_page(_by)
 })
 
 // if url has attributes (persistent share) render accordingly
